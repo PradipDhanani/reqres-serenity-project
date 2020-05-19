b11cf1882c2f5c7226386b736c977ccd7e348b11
PradipDhanani <pradip.dhanani@gmail.com>
2020-05-19 22:39:29 +0100

    reqres-serenity-project

---
+++ src/main/java/com/req/res/utils/TestUtils.java
@@ -0,0 +1,14 @@
+package com.req.res.utils;
+
+import java.util.Random;
+
+/**
+ * Created by Pradip
+ */
+public class TestUtils {
+        public static String getRandomValue() {
+            Random random = new Random();
+            int randomInt = random.nextInt(100000);
+            return Integer.toString(randomInt);
+        }
+}
